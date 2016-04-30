5c509c87ff9d2f2f63b2858528748e69fdfa0961
Josja <josja@20hz.nl>
2016-04-30 16:45:49 +0200

    Initial Commit

---
+++ NICodeTask/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NICodeTask
+//
+//  Created by Josja Krosenbrink on 30/04/16.
+//  Copyright (c) 2016 20Hz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
